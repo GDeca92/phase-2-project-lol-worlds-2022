dfa7bb28b6f8632af1c74618ae161803c0b5efb1
Tyler Hall <thall2980@gmail.com>
2022-10-03 15:49:41 -0400

    db json for real

---
+++ my-app/src/components/CommentContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+const CommentContainer = () => {
+  return (
+    <div>
+      <Comment />
+    </div>
+  );
+};
+
+export default CommentContainer;
---
+++ my-app/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CommentContainer from "./CommentContainer";
+
+const Homepage = () => {
+  return (
+    <>
+      <div>
+        <h1>About</h1>
+      </div>
+      <CommentContainer />
+    </>
+  );
+};
+
+export default Homepage;
