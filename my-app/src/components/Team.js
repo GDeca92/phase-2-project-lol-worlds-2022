d87acd4d5d9dfe1ef31405c67ac398ce535222b4
Tyler Hall <thall2980@gmail.com>
2022-10-03 17:05:37 -0400

    team data fetched with props sent to group component

--- my-app/src/components/Team.js
+++ my-app/src/components/Team.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Team = () => {
-  return <div>Team</div>;
+const Team = ({ team }) => {
+  return (
+    <div>
+      <h1>{team.name}</h1>
+      <img src={team.image} alt={team.name} />
+      <h3>{team.region}</h3>
+      <p>Group: {team["group stage"]}</p>
+      <ul>
+        Roster
+        <li>Top: {team.Top}</li>
+        <li>Jungle: {team.Jungle}</li>
+        <li>Mid: {team.Mid}</li>
+        <li>ADC: {team.ADC}</li>
+        <li>Support: {team.Support}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Team;
