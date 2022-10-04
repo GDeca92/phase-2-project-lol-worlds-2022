923274d70ae1df21c63e3656619a27170c659f8c
Tyler Hall <thall2980@gmail.com>
2022-10-04 12:28:14 -0400

    css for teams

--- my-app/src/components/CommentContainer.js
+++ my-app/src/components/CommentContainer.js
@@ -1,21 +1,16 @@
 import React from "react";
 import Comment from "./Comment";
 
-const CommentContainer = ({commentsList}) => {
+const CommentContainer = ({ commentsList }) => {
   const commentComponent = commentsList.map((comment) => (
-  <Comment 
-    key={comment.id}
-    username={comment.username}
-    comments={comment.comments}
+    <Comment
+      key={comment.id}
+      username={comment.username}
+      comments={comment.comments}
     />
-  ))
+  ));
 
-  return (
-    <div>
-      {commentComponent}
-    </div>
-  );
-
+  return <div>{commentComponent}</div>;
 };
 
 export default CommentContainer;
